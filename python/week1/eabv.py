720da092ca6f3e3546929fa27d4dd0de209b9a25
Estefany Benítez <azupbr26@gmail.com>
2025-02-02 20:21:54 -0600

    eabv-py

---
+++ python/week1/eabv.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 28 20:40:03 2025
+
+@author: MINEDUCYT
+
+# 1 WEEK
+
+Your programming challenge is to create a program that:
+
+Takes two inputs from the user
+
+First input: An integer (whole number like 1, 2, 3)
+Second input: A float (decimal number like 1.5, 2.7, 3.9)
+
+Multiply these two numbers together
+Display the result of the multiplication
+"""
+
+NUMBER1 = float (input("Ingrese numero decimal : " ))
+
+NUMBER2 = int (input("Ingrese numero entero : " )) 
+
+MULTI = NUMBER1 * NUMBER2 
+
+print("\n")
+
+print( "Resultado de Multiplicacion : ", MULTI)
